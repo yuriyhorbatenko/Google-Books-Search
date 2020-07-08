6765f872c39e59003547be6b465a2e0e30362e0d
Yuriy <xett03@gmail.com>
2020-07-08 10:39:56 -0400

    working on components

---
+++ README.md
@@ -0,0 +1,9 @@
+# Google-Books-Search
+
+**Google-Books-Search**
+
+**Unit 20: React Assignment**
+
+**Languages & Technologies used:**
+
+HTML, CSS, Javascript, Jquery, React, Node 
---
+++ client/src/pages/Search.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Form from "../components/Form";
+import List from "../components/List";
+import API from "../utils/API";
+
+class Search extends Component {
+  state = {
+    books: [],
+    value: "",
+  };
+
+  componentDidMount() {
+    this.searchBooks();
+  }
+
+  createBook = bookData => {
+    return {
+      _id: bookData.id,
+      title: bookData.volumeInfo.title,
+      authors: bookData.volumeInfo.authors,
+      description: bookData.volumeInfo.description,
+      image: bookData.volumeInfo.imageLinks.thumbnail,
+      link: bookData.volumeInfo.previewLink
+    }
+  }
+
+  searchBooks = () => {
+    API.getBook(query)
+      .then(res => this.setState({ books: res.data.items.map(bookData => this.createBook(bookData)) }))
+      .catch(err => console.error(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchBooks(this.state.search);
+  };
+
+  render() {
+    return (
+      <div>
+        <Form
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+        <div className="container">
+          <h2>Results</h2>
+          <List books={this.state.books} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Search;
